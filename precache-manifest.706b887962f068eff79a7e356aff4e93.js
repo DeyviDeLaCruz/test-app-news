6ca2f1e53505d0f4d87aa845b4a842739906b24f
Max Houston Ramirez Martel <maxr523@gmail.com>
2020-08-31 19:45:25 -0500

    Initial dist subtree commit

---
+++ precache-manifest.706b887962f068eff79a7e356aff4e93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890bf9724339fd895b9e",
+    "url": "/test-app-news/css/app.4df800b7.css"
+  },
+  {
+    "revision": "77fca661701152622cc8",
+    "url": "/test-app-news/css/chunk-vendors.97989550.css"
+  },
+  {
+    "revision": "f0c1f17ab92959bdcd33db10faa9028c",
+    "url": "/test-app-news/index.html"
+  },
+  {
+    "revision": "890bf9724339fd895b9e",
+    "url": "/test-app-news/js/app.3bf68d0d.js"
+  },
+  {
+    "revision": "77fca661701152622cc8",
+    "url": "/test-app-news/js/chunk-vendors.951a02bc.js"
+  },
+  {
+    "revision": "99f4d6ea6ed5507d0e147413da3c9918",
+    "url": "/test-app-news/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-app-news/robots.txt"
+  }
+]);
